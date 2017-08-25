55caba9b59b3d673d1c604a4bd8387a500b79aa5
Isan-Rivkin <isanrivkin@gmail.com>
2017-08-26 00:08:48 +0300

    FIXED MAIN BUGS

--- src/API/Protocol.h
+++ src/API/Protocol.h
@@ -16,7 +16,7 @@
  ********************************************************************************/
 #define SERVER_IP "127.0.0.1"
 #define NEW_USER_LOGGED_IN 10
-#define SERVER_PORT 3399
+#define SERVER_PORT 5252
 #define MAIN_ERROR_UNKOWN 40
 // sent to matchier and busy both to exit completley
 #define MAIN_CLIENT_EXIT 150
